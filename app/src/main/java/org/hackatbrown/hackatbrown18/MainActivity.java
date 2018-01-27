725a8fff6ee93049080c18c5c8d532eaa54e1c0a
Christine <christine_chapman@brown.edu>
2018-01-27 14:53:18 -0500

    Initial Hack at Brown commit

---
+++ app/src/main/java/org/hackatbrown/hackatbrown18/MainActivity.java
@@ -0,0 +1,25 @@
+package org.hackatbrown.hackatbrown18;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
